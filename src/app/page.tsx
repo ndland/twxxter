ee75d321b211d3ba9955978bf0a9f86d7cddb3c6
Nicholas Land <ndland@users.noreply.github.com>
2023-12-08 19:39:30 +0000

    fix(storybook): fix issue with story not being correct variant
    This is now using CSF 3.0 according to https://storybook.js.org/docs/writing-stories.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,6 @@ import Tweet from "../components/tweet/Tweet";
 
 export default function Home() {
   return (
-    <Tweet profilePic="https://images.unsplash.com/photo-1529665253569-6d01c0eaf7b6?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D" />
+    <Tweet profilePic="https://images.unsplash.com/photo-1529665253569-6d01c0eaf7b6?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D" content="Hello!" />
   );
 }
--- src/components/tweet/Tweet.stories.tsx
+++ src/components/tweet/Tweet.stories.tsx
@@ -1,17 +1,28 @@
 // Tweet.stories.tsx
-import React from "react";
-import { Story, Meta } from "@storybook/react/types-6-0";
+import { Meta, StoryObj } from "@storybook/react";
 import Tweet from "./Tweet";
+import { LoremIpsum } from "lorem-ipsum";
 
-export default {
+const meta: Meta<typeof Tweet> = {
   title: "Components/Tweet",
   component: Tweet,
-} as Meta;
+};
 
-const Template: Story<{ message: string }> = (args) => <Tweet {...args} />;
+export default meta;
+type Story = StoryObj<typeof Tweet>;
 
-export const Default = Template.bind({});
-Default.args = {
-  profilePic:
-    "https://images.unsplash.com/photo-1529665253569-6d01c0eaf7b6?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D",
+export const Default: Story = {
+  args: {
+    profilePic:
+      "https://images.unsplash.com/photo-1529665253569-6d01c0eaf7b6?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D",
+    content: "Hello World",
+  },
+};
+
+export const LongTweet: Story = {
+  args: {
+    profilePic:
+      "https://images.unsplash.com/photo-1529665253569-6d01c0eaf7b6?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D",
+    content: new LoremIpsum().generateParagraphs(3),
+  },
 };
