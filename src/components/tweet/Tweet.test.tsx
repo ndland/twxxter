c5861f0e65d43579adfbd7a07d729165b1c04324
Nicholas Land <ndland@users.noreply.github.com>
2023-12-08 19:41:54 +0000

    feat(tweet): add ability to write content for a tweet

--- src/components/tweet/Tweet.test.tsx
+++ src/components/tweet/Tweet.test.tsx
@@ -1,15 +1,16 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { RenderResult, render } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Tweet from "./Tweet";
 
 describe("Tweet", () => {
   const imageUrl =
     "https://images.unsplash.com/photo-1529665253569-6d01c0eaf7b6?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D";
+  const tweet = "Hello World";
   let renderResult: RenderResult;
 
   beforeEach(() => {
-    renderResult = render(<Tweet profilePic={imageUrl} />);
+    renderResult = render(<Tweet profilePic={imageUrl} content={tweet} />);
   });
 
   it("should render without crashing", () => {
@@ -20,5 +21,10 @@ describe("Tweet", () => {
   it("should display the users profile image on the page", () => {
     const img = renderResult.getByAltText("Profile Picture");
     expect(img.src).toContain(encodeURIComponent(imageUrl));
+  });
+
+  it("should display the content of the tweet", () => {
+    const { container } = renderResult;
+    expect(container).toHaveTextContent("Hello World");
   });
 });
--- src/components/tweet/Tweet.tsx
+++ src/components/tweet/Tweet.tsx
@@ -3,17 +3,22 @@ import React from "react";
 
 interface TweetProps {
   profilePic: string;
+  content: string;
 }
 
-const Tweet: React.FC<TweetProps> = ({ profilePic }) => {
+const Tweet: React.FC<TweetProps> = ({ profilePic, content }) => {
   return (
-    <div className="relative w-16 h-16">
-      <Image
-        className="rounded-full"
-        src={profilePic}
-        alt="Profile Picture"
-        fill
-      />
+    <div className="flex items-center justify-between">
+      <div className="relative w-16 h-16 flex-shrink-0">
+        <Image
+          className="rounded-full h-full w-full object-cover absolute"
+          src={profilePic}
+          alt="Profile Picture"
+          width={100} // note: see https://nextjs.org/docs/app/building-your-application/optimizing/images#remote-images for more info
+          height={100}
+        />
+      </div>
+      <div>{content}</div>
     </div>
   );
 };
